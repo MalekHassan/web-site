73a16ff9fdbc3ac552d79ec69c9ede7cd81a8faa
Jane Smith <example@email.com>
2020-06-23 22:29:20 +0300

    Adding Java script for lab06

---
+++ js/add-content.js
@@ -0,0 +1,22 @@
+'use strict';
+var carname = '';
+var cartype='';
+var carversouin='';
+carname = prompt("Enter your car brand!");
+    window.confirm('Nice brand')
+var cartype = prompt("Is you car type an electrical car or Gas?")
+if (cartype=="electrical") {
+    window.confirm('Will done keep it Green ,LIKE THAT!')}
+   else { window.confirm ('So It is your chance to use an Electrical one!');}
+var carversouin = prompt("Is your Car old or new?")
+if (carversouin=="old") {
+    window.confirm('So,It is time to buy a new one')}
+   else { window.confirm ('Check-out our new cars');}
+
+   var txt;
+var r = confirm("Do you like Cars");
+if (r == true) {
+    window.confirm( 'we agreed with you')
+} else {
+    window.confirm( "You pressed Cancel! so let us go to the website");
+}
