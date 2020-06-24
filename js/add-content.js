890b1b8561503c5d4729456339cf9e67e637ad44
Jane Smith <example@email.com>
2020-06-24 11:45:37 +0300

    adding functions to the JS

--- js/add-content.js
+++ js/add-content.js
@@ -2,17 +2,26 @@
 var carname = '';
 var cartype='';
 var carversouin='';
-carname = prompt("Enter your car brand!");
-    window.confirm('Nice brand')
-var cartype = prompt("Is you car type an electrical car or gas?")
-if (cartype=="electrical") {
-    window.confirm('Will done keep it Green ,LIKE THAT!')}
-   else { window.confirm ('So It is your chance to use an Electrical one!');}
-var carversouin = prompt("Is your Car old or new?")
-if (carversouin=="old") {
-    window.confirm('So,It is time to buy a new one')}
-   else { window.confirm ('Check-out our new cars');}
+function first(){
+    carname = prompt("Enter your car brand!");
+    window.confirm('Nice brand');
+}
+first();
+function second(){
+    var cartype = prompt("Is you car type an electrical car or gas?")
+    if (cartype=="electrical") {
+        window.confirm('Will done keep it Green ,LIKE THAT!')}
+       else { window.confirm ('So It is your chance to use an Electrical one!');}
+ }
+ second();
+     function third(){
+    var carversouin = prompt("Is your Car old or new?")
+    if (carversouin=="old") {
+        window.confirm('So,It is time to buy a new one')}
+       else { window.confirm ('Check-out our new cars');}    
 
+ }
+ third()   
    var txt;
 var r = confirm("Do you like Cars");
 if (r == true) {
@@ -20,3 +29,5 @@ if (r == true) {
 } else {
     window.confirm( "You pressed Cancel! so let us go to the website");
 }
+
+
